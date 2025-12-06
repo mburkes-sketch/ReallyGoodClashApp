b5178e4d57d046e41ef212a810d42b980704e597
Max-Burkes <burkesmax@gmail.com>
2025-12-06 14:51:33 -0500

    cleaned up client.py

--- flask_app/client.py
+++ flask_app/client.py
@@ -1,15 +1,5 @@
 import requests
 
-API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjRiMjE3YTZlLTZlYjQtNDk1NC1iNTdmLTBjNTRmODhiMWVmMSIsImlhdCI6MTc2NTAzNzY2MCwic3ViIjoiZGV2ZWxvcGVyL2EyYzJjOTVhLWE2MzAtYTEyZC1hZmY4LTBlNjJjZThhODM2NCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI1MC4xNDQuMjEzLjIyNiJdLCJ0eXBlIjoiY2xpZW50In1dfQ.LcgiQGWGQa72eGuSX1ATkUxsL1xewxqcZzwTpv3hGyQ1T6usn4UPU0gSKXPWEVus0lBHrrpEhFcwjT3-mptc-w"
-
-import requests
-
-
-# 2. Define the Base URL and the specific endpoint
-BASE_URL = 'https://api.clashroyale.com/v1'
-PLAYER_TAG = '#8QJJJ0C'  # Example Player Tag (Must start with #)
-ENDPOINT = '/cards' 
-
 
 class ClasherClient(object):
     def __init__(self):
