8500e77396f4d288b37c575b52b87ae23f150cc1
ragify12 <raghav.paladugu@gmail.com>
2025-12-17 11:36:00 -0500

    minor changes

--- flask_app/models.py
+++ flask_app/models.py
@@ -6,6 +6,7 @@ def load_user(user_id):
     return User.objects(username=user_id).first()
 
 class User(db.Document, UserMixin):
+    # user info (from registration form)
     username = db.StringField(required=True, unique=True, min_length=1, max_length=40)
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True)
@@ -15,7 +16,8 @@ class User(db.Document, UserMixin):
         return self.username
 
 class Deck(db.Document):
+    # Links deck to user who created it
     author = db.ReferenceField(User, required=True)
     title = db.StringField(required=True, min_length=1, max_length=50)
     description = db.StringField(max_length=200)
-    cards = db.ListField(db.StringField(), max_length=8, min_length=8, required=True)
+    cards = db.ListField(db.StringField(), max_length=8, min_length=8, required=True)     # stores 8 card IDs as list of strings
